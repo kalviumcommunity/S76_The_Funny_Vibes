31adf14ffe1592bc4f5d1c6f1e7b199ceb4d8fc8
narendrakumar57 <narendrakumarsuresh57@gmail.com>
2025-02-06 17:13:13 +0530

    done

--- database.js
+++ database.js
@@ -1,14 +1,23 @@
 const mongoose = require("mongoose");
-const connectDatabase = ()=>{
-    mongoose
-    .connect(process.env.db_URL)
-    .then((data)=>{
-        console.log(`MongoDB connected with server: ${data.connection.host}`)
+require('dotenv').config(); // to load the environment variables from .env file
+
+const connectDatabase = () => {
+  const dbURL = process.env.db_URL; // Fetching database URL from environment variable
+
+  if (!dbURL) {
+    console.log("Database connection URL is not defined in environment variables.");
+    process.exit(1); // If the URL isn't found, terminate the application
+  }
+
+  mongoose
+    .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((data) => {
+      console.log(`MongoDB connected with server: ${data.connection.host}`); // Successful connection message
     })
-    .catch((err)=>{
-        console.log(`Database connectio failed: ${err.message}`)
-        process.exit(1)
-    })
-}
+    .catch((err) => {
+      console.log(`Database connection failed: ${err.message}`); // Error message if the connection fails
+      process.exit(1); // Terminate the application if the connection fails
+    });
+};
 
 module.exports = connectDatabase;
--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-require('dotenv').config(); 
+// require('dotenv').config(); 
 
-const express = require('express');
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3000;
+// // const dotenv =require("dotenv");
+// const connectDatabase = require('./database');
+
+//
+
+// connectDatabase();
+
+// app.listen(port, () => {
+//     console.log(`Server is running at http://localhost:${port}`);
+// });
+
+const express = require("express");
+const connectDatabase = require("./database");
+
 const app = express();
-const port = process.env.PORT || 3000;
-const dotenv =require("dotenv");
-const connectDatabase = require('./database');
-
-app.get('/welcome', (req, res) => {
-    res.send('Welcome to The Funny Vibes!');
-});
+const PORT = process.env.PORT || 5000;
 
 connectDatabase();
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.get('/welcome', (req, res) => {
+        res.send('Welcome to The Funny Vibes!');
+    });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
